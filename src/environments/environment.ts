011451fe3dbf264634c2ddaa671ce2347ca5b2d1
The Rubyist <49131888+ruby-zen@users.noreply.github.com>
2021-08-25 16:02:39 +0530

    Updated environment file
    Updated environment file with firebase configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,14 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyDIDaU2NvNIeHq69f8ryXUha4XbbKprR7I",
+    authDomain: "angular-auth-650c7.firebaseapp.com",
+    projectId: "angular-auth-650c7",
+    storageBucket: "angular-auth-650c7.appspot.com",
+    messagingSenderId: "60534352026",
+    appId: "1:60534352026:web:ce664c4a694ad5b9305723",
+    measurementId: "G-9Z8MWMWLLF"
+  },
 };
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
